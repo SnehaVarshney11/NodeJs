73fbd1bff6505514ae88bd9a85d18ab1bb0647b6
sneha <sneha.varshney_cs19@gla.ac.in>
2024-05-12 23:13:47 +0530

    auth file

--- DEMO/auth.js
+++ DEMO/auth.js
@@ -6,13 +6,13 @@ const bcrypt = require('bcrypt');
 passport.use(new LocalStrateg(async (username, password, done) => {
     //Authentication Logic
     try{
-        console.log('Recieved Credentials', username, password);
-        const user = await Person.findOne({ USERNAME: username });
+        //console.log('Recieved Credentials', username, password);
+        const user = await Person.findOne({ username });
         if(!user) {
             return done(null, false, { message: 'Incorrect Username' });
         }
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if(passwordMatch) {
+        const isPwdMatch = await user.comparePassword(password);
+        if(isPwdMatch) {
             return done(null, user);
         }else {
             return done(null, false, { message: 'Incorrect Password' });
