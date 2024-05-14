f5007dc45e833d63e9cb6c1720bb8a9a66bc53e7
sneha <sneha.varshney_cs19@gla.ac.in>
2024-05-14 11:54:49 +0530

    username and password added for authentication

--- DEMO/models/Person.js
+++ DEMO/models/Person.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 //Define person schema
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    },
+    }, 
     age: {
         type: Number
     }, 
@@ -22,8 +23,47 @@ const personSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true 
+    },
+    username: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true,
     }
 })
+
+personSchema.pre('save', async function(next){
+    const person = this;
+
+    //if password is already in use
+    if(!person.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10); // generate the salt
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        //override plain password with hashed password 
+        person.password = hashedPassword;
+        next();
+    }catch(err) {
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePwd) {
+    try{
+        //compare method extract the salt from the actual password then create a new pwd with enterd pass + salt then it will compare
+        const isMatch = await bcrypt.compare(candidatePwd, this.password);
+        return isMatch;
+    }catch(err) {
+        throw err;
+    }
+}
 
 //Create Person Model
 const Person = mongoose.model('Person', personSchema);
