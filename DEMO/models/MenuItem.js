bc81cd556ad433eb84c24c598acde35f22083112
sneha <sneha.varshney_cs19@gla.ac.in>
2024-05-20 00:22:15 +0530

    authentication added

--- DEMO/models/MenuItem.js
+++ DEMO/models/MenuItem.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const menuItemSchema = new mongoose.Schema({
     name:{
@@ -26,8 +27,46 @@ const menuItemSchema = new mongoose.Schema({
     num_sales: {
         type: Number,
         required: true 
+    },
+    username: {
+        type: String,
+        required: true 
+    },
+    password: {
+        type: String,
+        required: true 
     }
 })
+
+menuItemSchema.pre('save', async function(next){
+    const menuItem = this;
+
+    //if password is already in use
+    if(!menuItem.isModified('password')) return next();
+
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10); // generate the salt
+
+        //hash password
+        const hashedPassword = await bcrypt.hash(menuItem.password, salt);
+
+        //override plain password with hashed password 
+        menuItem.password = hashedPassword;
+        next();
+    }catch(err) {
+        return next(err);
+    }
+})
+
+menuItemSchema.methods.compareMenuPassword = async function(candidatePwd) {
+    try{
+        const isPassMatch = await bcrypt.compare(candidatePwd, this.password);
+        return isPassMatch;
+    }catch(err) {
+        throw err;
+    }
+}
 
 const MenuItem = mongoose.model('MenuItem', menuItemSchema);
 module.exports = MenuItem;
