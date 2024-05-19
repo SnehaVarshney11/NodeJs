0639953da34c961d5a02112203b1ca14c6e1d422
sneha <sneha.varshney_cs19@gla.ac.in>
2024-05-20 00:21:56 +0530

    server file

--- DEMO/server.js
+++ DEMO/server.js
@@ -1,41 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const passport = require('./auth.js');
-const { connectToDatabase } = require('./db.js');
+const bodyParser = require("body-parser");
+const passport = require("./auth.js");
+const { connectToDatabase } = require("./db.js");
 app.use(bodyParser.json());
 app.use(passport.initialize());
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 connectToDatabase();
 
 const logRequest = (req, res, next) => {
-    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
-    next();
+  console.log(
+    `[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`
+  );
+  next();
 };
 app.use(logRequest);
 
 // Routes
-const personRoutes = require('./routes/personRoutes.js');
-const menuRoutes = require('./routes/menuRoutes.js');
-const milkRoutes = require('./routes/milkRoutes.js');
+const personRoutes = require("./routes/personRoutes.js");
+const menuRoutes = require("./routes/menuRoutes.js");
+const milkRoutes = require("./routes/milkRoutes.js");
 
-const localAuthMiddleware = passport.authenticate('local', { session: false });
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-app.use('/person', localAuthMiddleware, personRoutes);
-app.use('/menu', menuRoutes);
-app.use('/milk', milkRoutes);
+app.use("/person", personRoutes);
+app.use("/menu", localAuthMiddleware, menuRoutes);
+app.use("/milk", milkRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
 
 /*
 -------------DEMO--------------------------
